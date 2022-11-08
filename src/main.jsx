189e36503aeadf2f2cb4d5c2296516b06f78badf
paszo <paszo2008@gmail.com>
2022-11-08 14:09:10 +0100

    added local error

--- src/main.jsx
+++ src/main.jsx
@@ -41,6 +41,7 @@ const router = createBrowserRouter([
         {
             path: "contacts/:contactId/destroy",
             action: destroyAction,
+            errorElement: <div>Oops! There was an error.</div>
         }
     ],
   },
