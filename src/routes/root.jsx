3c34e5558437e1cc2816736f7e3a2f91d1f79903
paszo <paszo2008@gmail.com>
2022-11-08 12:06:14 +0100

    changed the links to be navlinks

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -1,13 +1,15 @@
 import {
   Outlet,
-  Link,
+    NavLink,
   useLoaderData,
-    Form
+  Form,
+  redirect
 } from "react-router-dom";
 import { getContacts, createContact} from "../contacts";
 
 export async function action() {
-  await createContact();
+  const contact = await createContact();
+  return redirect(`/contacts/${contact.id}/edit`)
 }
 
 export async function loader() {
@@ -52,7 +54,15 @@ export default function Root() {
             <ul>
               {contacts.map((contact) => (
                 <li key={contact.id}>
-                  <Link to={`contacts/${contact.id}`}>
+                  <NavLink to={`contacts/${contact.id}`}
+                           className={({isActive, isPending}) =>
+                               isActive ?
+                                   "active" :
+                                   isPending ?
+                                       "pending"
+                                       : ""
+                           }
+                  >
                     {contact.first || contact.last ? (
                       <>
                         {contact.first} {contact.last}
@@ -61,7 +71,7 @@ export default function Root() {
                       <i>No Name</i>
                     )}{" "}
                     {contact.favorite && <span>★</span>}
-                  </Link>
+                  </NavLink>
                 </li>
               ))}
             </ul>
