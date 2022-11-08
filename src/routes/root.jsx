e20decf842461e825f25fd8b282573ad0f60f425
paszo <paszo2008@gmail.com>
2022-11-08 14:00:12 +0100

    added loading effect

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -3,7 +3,8 @@ import {
     NavLink,
   useLoaderData,
   Form,
-  redirect
+  redirect,
+    useNavigation
 } from "react-router-dom";
 import { getContacts, createContact} from "../contacts";
 
@@ -21,6 +22,7 @@ export async function loader() {
 export default function Root() {
 
   const {contacts} = useLoaderData();
+  const navigation = useNavigation();
 
   return (
     <>
@@ -82,7 +84,7 @@ export default function Root() {
           )}
         </nav>
       </div>
-      <div id="detail">
+      <div id="detail" className={navigation.state === "loading" ? "loading" : ""}>
         <Outlet/>
       </div>
     </>
